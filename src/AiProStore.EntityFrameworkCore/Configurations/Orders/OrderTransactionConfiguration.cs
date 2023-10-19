0e050f4594c914d8acac5268cc416867b840429f
Hairicorn99 <mdhaic5tantrao2018@gmail.com>
2023-10-19 13:37:55 +0700

    data configuration

---
+++ src/AiProStore.EntityFrameworkCore/Configurations/Orders/OrderTransactionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AiProStore.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiProStore.Configurations.Orders
+{
+    public class OrderTransactionConfiguration : IEntityTypeConfiguration<OrderTransaction>
+    {
+        public void Configure(EntityTypeBuilder<OrderTransaction> builder)
+        {
+            builder.ToTable(AiProStoreConsts.DbTablePrefix + "OrderTransactions");
+
+            builder.Property(x => x.Code)
+                 .HasMaxLength(50)
+                 .IsUnicode(false)
+                 .IsRequired();
+        
+        }
+    }
+}
