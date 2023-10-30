d7f0e18eaab53d7c2666496fcaf6cabb83e21a17
Hairicorn99 <mdhaic5tantrao2018@gmail.com>
2023-10-30 09:01:54 +0700

    create product & product category api

--- src/AiProStore.Admin.Application/AiProStoreApplicationAutoMapperProfile.cs
+++ src/AiProStore.Admin.Application/AiProStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using AiProStore.Admin.ProductCategories;
+using AiProStore.ProductCategories;
+using AiProStore.Products;
 
 namespace AiProStore.Admin;
 
@@ -9,5 +12,14 @@ public class AiProStoreApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+        // Product category
+        CreateMap<ProductCategory, ProductCategoryDto>();
+        CreateMap<ProductCategory, ProductCategoryInListDto>();
+        CreateMap<CreateUpdateProductCategoryDto, ProductCategory>();
+
+        // Product
+        CreateMap<Product, ProductDto>();
+        CreateMap<Product, ProductInListDto>();
+        CreateMap<CreateUpdateProductDto, Product>();
     }
 }
